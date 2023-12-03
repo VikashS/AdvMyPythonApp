72313fd1a2df7a7f4cae5e94bfaf6ecd2367e25b
Vikash Singh <Glovesdown@gmail.com>
2023-12-03 14:45:43 +0100

    First Commit

---
+++ src/examples/file2.py
@@ -0,0 +1,10 @@
+def print_hello():
+    print("hi i am live now")
+
+
+class MyClass:
+    def __int__(self, word) -> None:
+        self.word = word
+
+    def print(self):
+        print(self.word)
